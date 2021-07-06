b29c99cd793db3d85d5480e2718a7e7f02515e68
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-07-06 09:56:05 +0200

    run action

---
+++ TypeWriter/Actions/Run/DuaLoader.ps1
@@ -0,0 +1,14 @@
+$RuntimePath = $args[0]
+$ProcessId = $args[1]
+$Location = $args[2]
+$FileName = $args[3]
+
+#$global:ProgressPreference = 'SilentlyContinue'
+
+Copy-Item -Path "$Location" -Destination "$RuntimePath/Cache/Run/$ProcessId/Archives/"
+
+Move-Item -Path "$RuntimePath/Cache/Run/$ProcessId/Archives/$FileName" -Destination "$RuntimePath/Cache/Run/$ProcessId/Archives/$FileName.zip"
+
+Expand-Archive -Path "$RuntimePath/Cache/Run/$ProcessId/Archives/$FileName.zip" -DestinationPath "$RuntimePath/Cache/Run/$ProcessId/UnpackCache/"
+
+Move-Item -Path "$RuntimePath/Cache/Run/$ProcessId/Archives/$FileName.zip" -Destination "$RuntimePath/Cache/Run/$ProcessId/Archives/$FileName"
