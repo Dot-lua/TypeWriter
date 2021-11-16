1a57020adb177be2dd1a67c1bf0d2bce4a3c97a6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-11-16 08:48:02 +0100

    add more vars

--- TypeWriter/Libs/LoadExecutionValues.lua
+++ TypeWriter/Libs/LoadExecutionValues.lua
@@ -1,10 +1,16 @@
 return function()
     _G.FS = require("fs")
     _G.Json = require("json")
+    _G.Timer = require("timer")
+    _G.Sleep = Timer.sleep
+    _G.Wait = function(Sec) return Sleep(Sec * 1000) end
+    _G.LOS = require("los")
 
     _G.Import = require("Run/Import")
     _G.LoadPackage = require("Run/LoadPackage")
     _G.LoadInternal = require("Run/LoadInternal")
     _G.CallEntrypoint = require("Run/CallEntrypoint")
     _G.Class = require("core").Object
+
+    table.ToString = require("Run/TableToString")
 end
