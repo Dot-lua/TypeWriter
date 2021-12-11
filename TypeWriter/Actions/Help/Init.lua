e08ed68057fa2edafb18070bfb82a62f4dd63810
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-12-11 18:12:04 +0100

    new help command

--- TypeWriter/Actions/Help/Init.lua
+++ TypeWriter/Actions/Help/Init.lua
@@ -1,11 +1,11 @@
 local Logger = require("Logger")
 
 return function(Args)
-    Logger.Info("Usage: TypeWriter [options] script.lua [arguments]")
-    print()
 
-    Logger.Info("Options:")
-    for i, v in pairs(RuntimeGlobal.Arguments) do
-        Logger.Info("-" .. i .. ", --" .. v)
+    local Lines = require(RuntimePath .. "/Libs/Split.lua")(require("FS").readFileSync(RuntimePath .. "/Actions/Help/Usage.txt"), "\r\n")
+
+    for Index, Line in pairs(Lines) do
+        Logger.Info(Line)
     end
+
 end
