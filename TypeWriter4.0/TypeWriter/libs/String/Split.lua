695138467f3447de1ed4c579b2c6e7a4fba60cee
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2022-03-15 13:10:31 +0100

    update some things

--- TypeWriter4.0/TypeWriter/libs/String/Split.lua
+++ TypeWriter4.0/TypeWriter/libs/String/Split.lua
@@ -1,6 +1,6 @@
 return function(TheString, Decr)
     Lines = {}
-    for s in TheString:gmatch("[^" .. Decr .. "]+") do
+    for s in string.gmatch(TheString, "[^" .. Decr .. "]+") do
         table.insert(Lines, s)
     end
     return Lines
