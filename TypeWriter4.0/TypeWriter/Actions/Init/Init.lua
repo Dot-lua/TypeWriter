e4dec652af092f82ee282c1cca794a0acff03e7c
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2022-04-05 15:11:08 +0200

    init

--- TypeWriter4.0/TypeWriter/Actions/Init/Init.lua
+++ TypeWriter4.0/TypeWriter/Actions/Init/Init.lua
@@ -1,5 +1,19 @@
 local FS = require("fs")
+local Bundle = require("luvi").bundle
 
 return function ()
-    
+    local function CreateFolder(Path, OutputPath)
+        local Tree = require("path").resolve(OutputPath)
+        for Index, Folder in pairs(string.Split(Path, "/")) do
+            FS.mkdirSync(Tree .. "/" .. Folder)
+            Tree = Tree .. "/" .. Folder
+        end        
+    end
+
+    local OutputPath = TypeWriter.ArgumentParser:GetArgument("output", "o", "./")
+
+    CreateFolder("src/Main/lua/ga/corebyte/template/", OutputPath)
+    CreateFolder("src/Main/resources/", OutputPath)
+    FS.writeFileSync(OutputPath .. "/src/Main/lua/ga/corebyte/template/Main.lua", "")
+    FS.writeFileSync(OutputPath .. "/src/Main/resources/package.info.lua", Bundle.readfile("/Actions/Init/package.info.lua"))
 end
---
+++ TypeWriter4.0/TypeWriter/Actions/Init/package.info.lua
@@ -0,0 +1,35 @@
+-- See https://github.com/Dot-lua/TypeWriter/wiki/package.info.lua-format for more info
+
+return { InfoVersion = 1, -- Dont touch this
+
+    ID = "template", -- A unique id 
+    Name = "Template",
+    Description = "A typewriter template",
+    Version = "1.0.0",
+
+    Author = {
+        Developers = {
+            "CoreByte"
+        },
+        Contributors = {}
+    },
+
+    Dependencies = {
+        Luvit = {
+            -- Luvit dependencies ("username/projectname")
+        },
+        Dua = {}
+    },
+
+    Contact = {
+        Website = "https://cubic-inc.ga",
+        Source = "https://github.com/Dot-lua/TypeWriter/",
+        Socials = {}
+    },
+
+    Entrypoints = {
+        Main = "nl.cubic-inc.template.Main"
+        -- CubyPackage = "some.other.entry.caused.by.another.package", -- a package can call another packages entrypoints
+    }
+
+}
