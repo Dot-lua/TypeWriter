179dfd3a83b4f4af8f30685c3aa8f22e0f506f8d
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-04-07 16:09:46 +0200

    not working script

---
+++ TypeWriter4.0/Build.ps1
@@ -0,0 +1,2 @@
+C:/luvit/luvi ./TypeWriter -o ./TypeWriter.exe
+cmd /C "`"C:/Program Files (x86)/Resource Hacker/ResourceHacker.exe`" -modify 'TypeWriter.exe', 'TypeWriter.exe', 'Icon.ico', ICONGROUP, MAINICON, 0"
