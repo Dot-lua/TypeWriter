1fa2cdd40f77d4cdc56e30685f52669b4b8fbec4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-04-09 17:12:33 +0200

    some updating

--- TypeWriter4.0/TypeWriter/Actions/Init/Init.lua
+++ TypeWriter4.0/TypeWriter/Actions/Init/Init.lua
@@ -2,6 +2,7 @@ local FS = require("fs")
 local Bundle = require("luvi").bundle
 
 return function ()
+
     local function CreateFolder(Path, OutputPath)
         local Tree = require("path").resolve(OutputPath)
         for Index, Folder in pairs(string.Split(Path, "/")) do
@@ -11,6 +12,11 @@ return function ()
     end
 
     local OutputPath = TypeWriter.ArgumentParser:GetArgument("output", "o", "./")
+
+    if FS.existsSync(OutputPath .. "/src/") then
+        TypeWriter.Logger.Error(OutputPath .. "/src/ already exists")
+        process:exit(1)
+    end
 
     CreateFolder("src/Main/lua/ga/corebyte/template/", OutputPath)
     CreateFolder("src/Main/resources/", OutputPath)
