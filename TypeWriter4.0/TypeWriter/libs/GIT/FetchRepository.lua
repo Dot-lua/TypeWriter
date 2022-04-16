2fd649d577d5250538f7b3ec77894a81299767f6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-04-16 11:37:54 +0200

    git deps support and branch support

--- TypeWriter4.0/TypeWriter/libs/GIT/FetchRepository.lua
+++ TypeWriter4.0/TypeWriter/libs/GIT/FetchRepository.lua
@@ -1,21 +1,60 @@
 local Spawn = require("coro-spawn")
 local FS = require("fs")
+local Lit = require("LIT/FetchPackage")
 
 return function (Name)
-    if FS.existsSync(TypeWriter.Folder .. "/PackageCache/" .. string.split(Name, "/")[2]) then
+    local Author = string.split(Name, "/")[1]
+    local Repository = string.split(string.split( Name, "/")[2], "@")[1]
+    local Branch = string.split(string.split(Name, "/")[2], "@")[2]
+
+    p(Author)
+    p(Repository)
+    p(Branch)
+
+    if FS.existsSync(TypeWriter.Folder .. "/PackageCache/" .. Repository) then
         return
     end
 
     TypeWriter.Logger.Info("Downloading " .. Name)
+
+    local Arguments = {
+        "clone",
+        "https://github.com/" .. Author .. "/" .. Repository
+    }
+
+    if Branch then
+        table.insert(Arguments, "-b")
+        table.insert(Arguments, Branch)
+    end
+
+
     local Result, Error = Spawn(
         "git",
         {
             cwd = TypeWriter.Folder .. "/PackageCache/",
-            args = {
-                "clone",
-                "https://github.com/" .. Name
-            }
+            args = Arguments
         }
     )
     Result.waitExit()
+
+    if FS.existsSync(TypeWriter.Folder .. "/PackageCache/" .. Repository) then
+        TypeWriter.Logger.Info("Downloaded " .. Name)
+
+        if FS.existsSync(TypeWriter.Folder .. "/PackageCache/" .. Repository .. "/package.lua") then
+            
+            local DownloadedPackage = load(
+                FS.readFileSync(
+                    TypeWriter.Folder .. "/PackageCache/" .. Repository .. "/package.lua"
+                )
+            )()
+
+            if DownloadedPackage.dependencies == nil then DownloadedPackage.dependencies = {} end
+
+            for Index, Dep in pairs(DownloadedPackage.dependencies) do
+                Lit(Dep)
+            end
+        end
+    else
+        TypeWriter.Logger.Error("Failed to download " .. Name)
+    end
 end
