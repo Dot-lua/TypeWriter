94e6954542c07efef5384eb9bda946653aea3059
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-18 18:33:08 +0200

    Create Build.ps1

---
+++ Scripts/Build.ps1
@@ -0,0 +1,7 @@
+cd ./TypeWriter4.0/
+mkdir ./Binary/
+
+curl --output ./luvit.zip --url https://github.com/truemedian/luvit-bin/releases/latest/download/luvit-bin-Windows-x86_64.zip
+tar.exe -xf ./luvit.zip -C ./
+
+ls
