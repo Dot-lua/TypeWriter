096c71e0de4ed136053d15e1b3439f10150120d3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-20 18:58:32 +0200

    show version in help

--- TypeWriter4.0/TypeWriter/Actions/Help/Help.lua
+++ TypeWriter4.0/TypeWriter/Actions/Help/Help.lua
@@ -3,4 +3,5 @@ local Bundle = require("luvi").bundle
 
 return function ()
     TypeWriter.Logger.Info(Bundle.readfile("/Actions/Help/Usage.txt"))
+    TypeWriter.Logger.Info("TypeWriter version " .. TypeWriter.Package.Version)
 end
