07bbe362b63456205001773c5b18855f0d81891f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-03 22:33:07 +0200

    delete build script

--- Scripts/Build.ps1
+++
@@ -1,7 +0,0 @@
-cd ./TypeWriter4.0/
-mkdir ./Binary/
-cd ./Binary/
-curl --output ./luvit.zip --url https://github.com/truemedian/luvit-bin/releases/latest/download/luvit-bin-Windows-x86_64.zip
-tar.exe -xf ./luvit.zip -C ./
-
-ls
