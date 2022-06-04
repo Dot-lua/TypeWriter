1b232375cafb7f89c565fb576dc1a04255d1061a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-04 12:48:45 +0200

    Fix updating

--- TypeWriter4.0/TypeWriter/Updater/GetReleases.lua
+++ TypeWriter4.0/TypeWriter/Updater/GetReleases.lua
@@ -3,7 +3,7 @@ local JsonRequest = require("../Installer/Request").JsonRequest
 return function ()
     local Response, Data = JsonRequest(
         "GET",
-        "https://api.github.com/repos/Dot-Lua/TypeWriter/releases",
+        "https://api.github.com/repos/Dot-Lua/TypeWriter/releases?per_page=10",
         {
             {"User-Agent", "TypeWriter"}
         }
