d6046a008bf12db68b6b8edc13ad18cd5ea0b6dd
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-21 21:57:06 +0200

    update main update function

--- TypeWriter/TypeWriter/Updater/Main.lua
+++ TypeWriter/TypeWriter/Updater/Main.lua
@@ -1,9 +1,5 @@
 local Package = require("../package")
 return function ()
-    TypeWriter.ProcessMonitor:GetInstancesOf("TypeWriter.exe")
-    if TypeWriter.SessionManager:GetRunningCount() ~= 0 then
-        return
-    end
     if not require("./HasInternet")() then
         return
     end
@@ -13,6 +9,11 @@ return function ()
         return
     end
     if Latest.tag_name == Package.version then
+        return
+    end
+    local Running = TypeWriter.ProcessMonitor:GetInstancesOf("TypeWriter.exe")
+    if Running ~= 1 then
+        TypeWriter.Logger.Error("Update available but there are instances running, Stop running instances to update! (%s running)", Running - 1)
         return
     end
     require("./Update")(Latest)
