acc0937024970abd5e2720fc4f8aa1b47172a155
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-02-05 22:32:04 +0100

    stuff

--- TypeWriter/Index.js
+++ TypeWriter/Index.js
@@ -1,12 +1,29 @@
 const Fengari = require("fengari")
+const Interop = require('fengari-interop');
 
-const luaconf  = Fengari.luaconf;
-const lua      = Fengari.lua;
-const lauxlib  = Fengari.lauxlib;
-const lualib   = Fengari.lualib;
+const lua = Fengari.lua
+const lauxlib = Fengari.lauxlib
+const lualib = Fengari.lualib
+const to_luastring = Fengari.to_luastring
 
-const L = lauxlib.luaL_newstate();
-
+let L = lauxlib.luaL_newstate();
 lualib.luaL_openlibs(L);
+lauxlib.luaL_requiref(L, to_luastring("js"), Interop.luaopen_js, 1);
+console.log(lua.lua_gettop(L))
 
-lua.lua_load(L, "print('ho')")
+
+if (!L) throw Error("failed to create lua state");
+global.hi = "ahas"
+
+let luaCode = `
+    local a = "hello world"
+    for I, V in pairs(require("js").global) do print(I, V) print("") end
+    print(require("js"))
+    print(a)
+    return a
+`;
+{
+    lauxlib.luaL_loadstring(L, to_luastring(luaCode));
+    lua.lua_call(L, 0, -1);
+}
+console.log(lua.lua_tojsstring(L, -1))
