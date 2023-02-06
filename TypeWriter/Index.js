b766ac55b4e1dfd58e671b9294ffc6c86dd1894a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-02-06 21:57:35 +0100

    add lib

--- TypeWriter/Index.js
+++ TypeWriter/Index.js
@@ -1,29 +1,41 @@
 const Fengari = require("fengari")
 const Interop = require('fengari-interop');
+const Flua = require("flua")
 
 const lua = Fengari.lua
 const lauxlib = Fengari.lauxlib
 const lualib = Fengari.lualib
 const to_luastring = Fengari.to_luastring
 
-let L = lauxlib.luaL_newstate();
-lualib.luaL_openlibs(L);
-lauxlib.luaL_requiref(L, to_luastring("js"), Interop.luaopen_js, 1);
-console.log(lua.lua_gettop(L))
+//Create lua scope
 
 
-if (!L) throw Error("failed to create lua state");
-global.hi = "ahas"
+global.TypeWriter = {}
+{
+    let L = lauxlib.luaL_newstate();
+    lualib.luaL_openlibs(L);
+    lauxlib.luaL_requiref(L, to_luastring("js"), Interop.luaopen_js, 1);
+    if (!L) throw Error("failed to create lua state");
+    global.TypeWriterLuaState = L
+}
 
+Flua.flua_setglobals(
+    TypeWriterLuaState,
+    {
+        TypeWriter: global.TypeWriter
+    }
+)
+
+TypeWriter.b = "asd"
 let luaCode = `
     local a = "hello world"
-    for I, V in pairs(require("js").global) do print(I, V) print("") end
     print(require("js"))
     print(a)
+    print(TypeWriter.b)
     return a
 `;
 {
-    lauxlib.luaL_loadstring(L, to_luastring(luaCode));
-    lua.lua_call(L, 0, -1);
+    lauxlib.luaL_loadstring(TypeWriterLuaState, to_luastring(luaCode));
+    lua.lua_call(TypeWriterLuaState, 0, -1);
 }
-console.log(lua.lua_tojsstring(L, -1))
+console.log(lua.lua_tojsstring(TypeWriterLuaState, -1))
