2838e2cc6b10d1cad62f932a1b5aed0a7938ec2a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-02-26 22:55:46 +0100

    replace and fill list.json

---
+++ TypeWriter/Actions/List.js
@@ -0,0 +1,6 @@
+module.exports = {
+    build:      require("./build/Index.js"),
+    execute:    require("./execute/Index.js"),
+    new:        require("./new/Index.js"),
+    run:        require("./run/Index.js"),
+}
