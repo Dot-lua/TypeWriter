15430c4f2d19569b5ebe22a80fbcad53e75e5b75
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-02-26 22:56:20 +0100

    finish build action

--- TypeWriter/Actions/build/Index.js
+++ TypeWriter/Actions/build/Index.js
@@ -2,3 +2,13 @@
 module.exports.Name = "Build"
 module.exports.Execute = function() {
     const InputPath = TypeWriter.Arguments.input
+    const InputBranch = TypeWriter.Arguments.branch
+
+    const BuildHelper = require("../../Lib/BuildHelper.js")
+
+    console.log(InputPath)
+
+    const BuildId = BuildHelper.Build(InputPath, InputBranch)
+    BuildHelper.CompressBuild(BuildId, TypeWriter.Arguments.output)
+    BuildHelper.CleanupBuild(BuildId)
+}
