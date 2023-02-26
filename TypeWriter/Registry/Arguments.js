8cf1a91ffa10eaeb94fc67df6f94b567270a30e4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-02-26 22:56:59 +0100

    rework arguments registry

--- TypeWriter/Registry/Arguments.js
+++ TypeWriter/Registry/Arguments.js
@@ -18,18 +18,75 @@ MainParser.add_argument(
 const SubParsers = MainParser.add_subparsers(
     {
         title: "action",
-        description: "action to execute"
+        description: "action to execute",
+        dest: "action"
     }
 )
 
-const BuildParser = SubParsers.add_parser("build", {help: "build a twr archive"})
-BuildParser.add_argument(
-    "-i", "--input",
-    {
-        help: "input file"
-    }
-)
+{ //Build Parser
+    const BuildParser = SubParsers.add_parser(
+        "build",
+        {
+            help: "build a twr archive"
+        }
+    )
+    BuildParser.add_argument(
+        "-i", "--input",
+        {
+            help: "input file",
+            metavar: "path",
+            default: "./src/"
+        }
+    )
+    BuildParser.add_argument(
+        "-b", "--branch",
+        {
+            help: "input branch",
+            metavar: "branch",
+            default: "Main"
+        }
+    )
+    BuildParser.add_argument(
+        "-o", "--output",
+        {
+            help: "output folder",
+            metavar: "path",
+            default: "./"
+        }
+    )
+}
+{ //Execute Parser
+    const ExecuteParser = SubParsers.add_parser(
+        "execute",
+        {
+            help: "execute a twr archive"
+        }
+    )
+    ExecuteParser.add_argument(
+        "-i", "--input",
+        {
+            help: "input file",
+            metavar: "path",
+            required: true
+        }
+    )
+}
+{ //New Parser
+    const NewParser = SubParsers.add_parser(
+        "new",
+        {
+            help: "create a new project in the current cwd"
+        }
+    )
+}
+{ //Run Parser
+    const RunParser = SubParsers.add_parser(
+        "run",
+        {
+            help: "run the src in the cwd"
+        }
+    )
+}
 
 const Parsed = MainParser.parse_args()
-console.log(Parsed)
 module.exports = Parsed
