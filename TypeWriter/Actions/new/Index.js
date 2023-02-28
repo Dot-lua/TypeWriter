29c8d91cb0ec5c4b54ddda8855ff7022006df150
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-02-28 15:11:26 +0100

    complete new action

--- TypeWriter/Actions/new/Index.js
+++ TypeWriter/Actions/new/Index.js
@@ -1,6 +1,19 @@
+const FS = require("fs-extra")
+
 module.exports.Name = "New"
 module.exports.Execute = function() {
-    const InputPath = TypeWriter.Arguments.input
+    if (FS.existsSync("./src/")) {
+        TypeWriter.Logger.Error("The ./src/ folder already exists, please remove it before trying again.")
+        process.exit(1)
+    }
 
-    console.log(InputPath)
+    FS.mkdirp("./src/Main/js/me/corebyte/template/")
+    FS.mkdirp("./src/Main/lua/me/corebyte/template/")
+    FS.mkdirp("./src/Main/resources/")
+    FS.copySync(require("path").join(__dirname, "../../Assets/package.info.json"), "./src/Main/package.info.json")
+    FS.writeFileSync("./src/Main/lua/me/corebyte/template/Main.lua", "print(\"Hello lua world\")")
+    FS.writeFileSync("./src/Main/js/me/corebyte/template/JsMain.js", "console.log(\"Hello js world\")")
+
+    TypeWriter.Logger.Information("The ./src/ is now created but you still need to edit the package.info.json.")
+
 }
