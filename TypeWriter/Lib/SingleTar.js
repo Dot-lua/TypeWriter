c7bd77285400a8530c31cb743115af685b55f0d4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-05 21:50:14 +0100

    Create SingleTar.js

---
+++ TypeWriter/Lib/SingleTar.js
@@ -0,0 +1,18 @@
+//https://github.com/isaacs/node-tar/issues/181#issuecomment-1153196779
+
+var tar = require('tar');
+
+function extractFileFromTarball(tarpath, filename) {
+  var nentries = 0;
+  var data = [];
+  var onentry = entry => {
+    nentries++;
+    if (nentries % 1e4 === 0) { console.log('nentries=' + nentries); }
+    if (entry.path === filename) { entry.on('data', c => data.push(c)); }
+  };
+  tar.t({onentry, file: tarpath, sync: true});
+  var buf = Buffer.concat(data);
+  return buf.toString('ascii');
+}
+
+module.exports = extractFileFromTarball
