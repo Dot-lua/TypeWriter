513c6eb1f18971999247d435ddb8d2d8f48a291a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-05 21:54:09 +0100

    new actions

--- TypeWriter/Actions/execute/Index.js
+++ TypeWriter/Actions/execute/Index.js
@@ -1,5 +1,21 @@
+const FS = require("fs-extra")
+const RandomString = require("randomstring")
+const Path = require("path")
+const Tar = require("tar")
+const RuntimeHelper = require("../../Lib/RuntimeHelper")
+
 module.exports.Name = "Execute"
 module.exports.Execute = function() {
     const InputPath = TypeWriter.Arguments.input
+    TypeWriter.Logger.Debug(`Input is ${InputPath}`)
+    const ExecuteId = RandomString.generate(32)
+    TypeWriter.Logger.Debug(`ExecuteId is ${ExecuteId}`)
+    const ExecuteFolder = Path.normalize(`${TypeWriter.Folder}/Cache/ExecuteCache/${ExecuteId}/`)
+    TypeWriter.Logger.Debug(`ExecuteFolder is ${ExecuteFolder}`)
+    
+    FS.mkdirSync(ExecuteFolder)
 
+    RuntimeHelper.LoadEnvoirment(ExecuteFolder)
+    const Package = TypeWriter.LoadFile(InputPath)
+    TypeWriter.LoadEntrypoint(Package.Id, "Main")
 }
--- TypeWriter/Actions/run/Index.js
+++ TypeWriter/Actions/run/Index.js
@@ -1,6 +1,28 @@
+const BuildHelper = require("../../Lib/BuildHelper.js")
+const RandomString = require("randomstring")
+const FS = require("fs-extra")
+const Path = require("path")
+const RuntimeHelper = require("../../Lib/RuntimeHelper")
+
 module.exports.Name = "Run"
 module.exports.Execute = function() {
     const InputPath = TypeWriter.Arguments.input
+    const InputBranch = TypeWriter.Arguments.branch
 
-    console.log(InputPath)
+
+    const BuildId = BuildHelper.Build(InputPath, InputBranch)
+    
+    const ExecuteId = RandomString.generate(32)
+    TypeWriter.Logger.Debug(`ExecuteId is ${ExecuteId}`)
+    const ExecuteFolder = Path.normalize(`${TypeWriter.Folder}/Cache/ExecuteCache/${ExecuteId}/`)
+    TypeWriter.Logger.Debug(`ExecuteFolder is ${ExecuteFolder}`)
+    
+    FS.mkdirSync(ExecuteFolder)
+
+    BuildHelper.CompressBuild(BuildId, ExecuteFolder)
+    BuildHelper.CleanupBuild(BuildId)
+
+    RuntimeHelper.LoadEnvoirment(ExecuteFolder)
+    const Package = TypeWriter.LoadFile(`${ExecuteFolder}/${FS.readdirSync(ExecuteFolder)}`)
+    TypeWriter.LoadEntrypoint(Package.Id, "Main")
 }
