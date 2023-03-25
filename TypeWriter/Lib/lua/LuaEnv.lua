3f54d8da208b651ea41108211e4aa4cc9acca19f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-25 12:04:26 +0100

    Update LuaEnv.lua

--- TypeWriter/Lib/lua/LuaEnv.lua
+++ TypeWriter/Lib/lua/LuaEnv.lua
@@ -12,40 +12,6 @@ do -- Set globals
     _G.JsNew = jsnew
 end
 
-do -- Require Fix
-    _G.require = function (Module)
-        if Module == "js" then
-            return js
-        end
-
-        --local M = OriginalRequire(Module)
-        --if M then
-        --    return M
-        --end
-
-        local S, M = pcall(
-            function ()
-                return js.global.process.mainModule:require("./Lib/RequireHelper.js"):RequireHelper(Module)
-            end
-        )
-        if M then
-            return M
-        end
-
-        local SnapshotFolder
-        if TypeWriter.OS == "win32" then
-            SnapshotFolder = "C:\\snapshot\\TypeWriter\\"
-        else
-            SnapshotFolder = "/snapshot/TypeWriter/"
-        end
-
-        if FS:existsSync(SnapshotFolder .. Module) then
-            return load(FS:readFileSync(SnapshotFolder .. Module, "utf8"))()
-        end
-
-    end
-end
-
 do -- Proxy functions
     _G.Import = function(Request)
         return TypeWriter:Import(Request)
