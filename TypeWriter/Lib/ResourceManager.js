b43168157198ce2cc1156c93fb7110c204c004a5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-13 15:44:16 +0200

    rework function names and path parsing

--- TypeWriter/Lib/ResourceManager.js
+++ TypeWriter/Lib/ResourceManager.js
@@ -2,43 +2,53 @@ const SingleTar = require("../Lib/SingleTar")
 const FS = require("fs-extra")
 const Path = require("path")
 
+const ResourceManager = {}
+
 function GetPackagePath(Id) {
     return `${TypeWriter.ExecuteFolder}/${Id}/`
 }
 
-function PackageLoaded(Id) {
-    return TypeWriter.LoadedPackages[Id] !== undefined
+function ParseResourcePath(Id, ResourcePath) {
+    if (!ResourcePath) {
+        const Split = Id.split(":")
+        return Split[0], Split[1]
+    }
+    return Id, ResourcePath
 }
 
-function ResourceExists(Id, ResourcePath) {
-    if (!PackageLoaded(Id)) {
+ResourceManager.ResourceExists = function(Id, ResourcePath) {
+    Id, ResourcePath = ParseResourcePath(Id, ResourcePath)
+    if (!TypeWriter.PackageManager.IsPackageLoaded(Id)) {
         return false
     }
     return TypeWriter.LoadedPackages[Id].ResourceIndex.includes(ResourcePath)
 }
 
-function ListResources(Id) {
-    if (!PackageLoaded(Id)) {
+ResourceManager.ListResources = function(Id) {
+    if (!TypeWriter.PackageManager.IsPackageLoaded(Id)) {
         return []
     }
     return TypeWriter.LoadedPackages[Id].ResourceIndex
 }
 
-function GetRaw(Id, ResourcePath) {
+ResourceManager.GetRaw = function(Id, ResourcePath) {
+    Id, ResourcePath = ParseResourcePath(Id, ResourcePath)
     if (!ResourceExists(Id, ResourcePath)) {
         return null
     }
     return SingleTar(TypeWriter.LoadedPackages[Id].PackagePath, `resources${ResourcePath}`)
 }
 
-function GetJson(Id, ResourcePath) {
+ResourceManager.GetJson = function(Id, ResourcePath) {
+    Id, ResourcePath = ParseResourcePath(Id, ResourcePath)
     if (!ResourceExists(Id, ResourcePath)) {
         return null
     }
     return JSON.parse(GetRaw(Id, ResourcePath))
 }
 
-function GetFilePath(Id, ResourcePath) {
+ResourceManager.GetFilePath = function(Id, ResourcePath) {
+    Id, ResourcePath = ParseResourcePath(Id, ResourcePath)
     if (!ResourceExists(Id, ResourcePath)) {
         return null
     }
@@ -49,10 +59,4 @@ function GetFilePath(Id, ResourcePath) {
     return OutputPath
 }
 
-module.exports = {
-    ResourceExists: ResourceExists,
-    ListResources: ListResources,
-    GetRaw: GetRaw,
-    GetJson: GetJson,
-    GetFilePath: GetFilePath
-}
+module.exports = ResourceManager
