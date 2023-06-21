03accc5376faf7140dc0103b559f53fddb2d839a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-06-22 00:16:56 +0200

    add some more comments

--- TypeWriter/Index.js
+++ TypeWriter/Index.js
@@ -18,6 +18,7 @@ TypeWriter.ApplicationData = `${TypeWriter.Folder}/ApplicationData/`
 TypeWriter.Logger = require("./Lib/Logger")
 TypeWriter.OriginalRequire = Module.prototype.require
 
+//Check if valid install
 if (FS.existsSync(`${TypeWriter.Folder}/InstallationDirectory`)) {
     TypeWriter.Logger.Debug("Valid installation found")
 } else {
@@ -26,19 +27,19 @@ if (FS.existsSync(`${TypeWriter.Folder}/InstallationDirectory`)) {
     process.exit(0)
 }
 
-{ //Create folders in Exe folder
-    FS.ensureDirSync(TypeWriter.ApplicationData)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/BuildCache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ExecuteCache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Modules/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/ModuleTars/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Unpack/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/LIT/`)
-}
+//Create folders in Exe folder
+FS.ensureDirSync(TypeWriter.ApplicationData)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/BuildCache/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ExecuteCache/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Modules/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/ModuleTars/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Unpack/`)
+FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/LIT/`)
 
+//Run action
 if (TypeWriter.Arguments.action) {
     const Actions = require("./Actions/List.js")
     const Action = Actions[TypeWriter.Arguments.action]
