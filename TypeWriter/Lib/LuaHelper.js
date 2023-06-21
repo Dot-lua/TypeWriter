4e8dfbb4cd00e38aec888d51bcb052b08eba7ae4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-06-21 23:35:11 +0200

    clean code

--- TypeWriter/Lib/LuaHelper.js
+++ TypeWriter/Lib/LuaHelper.js
@@ -1,5 +1,3 @@
-const LuaHelper = {}
-
 const Fengari = require("fengari")
 const Interop = require('fengari-interop');
 
@@ -8,39 +6,11 @@ const lauxlib = Fengari.lauxlib
 const lualib = Fengari.lualib
 const to_luastring = Fengari.to_luastring
 
-var ErrorFunc
-
 function CreateState() {
     let L = lauxlib.luaL_newstate();
     lualib.luaL_openlibs(L);
     lauxlib.luaL_requiref(L, to_luastring("js"), Interop.luaopen_js, 1);
-    lua.lua_pop(L, 1); /* remove lib */
-    if (!L) throw Error("failed to create lua state");
-
-    function luvi_traceback(L) {
-        if (!lua.lua_isstring(L, 1)) { /* 'message' not a string? */
-            return 1;  /* keep it intact */
-        } 
-        lua.lua_pushglobaltable(L);
-        lua.lua_getfield(L, -1, "debug");
-        lua.lua_remove(L, -2);
-        if (!lua.lua_istable(L, -1)) {
-            lua.lua_pop(L, 1);
-            return 1;
-        }
-        lua.lua_getfield(L, -1, "traceback");
-        if (!lua.lua_isfunction(L, -1)) {
-            lua.lua_pop(L, 2);
-            return 1;
-        }
-        lua.lua_pushvalue(L, 1);  /* pass error message */
-        lua.lua_pushinteger(L, 2);  /* skip this function and traceback */
-        lua.lua_call(L, 2, 1);  /* call debug.traceback */
-        return 1;
-    }
-
-    lua.lua_pushcfunction(L, luvi_traceback);
-    ErrorFunc = lua.lua_gettop(L);
+    lua.lua_pop(L, 1)
 
     lua.lua_atnativeerror(L, L => {
         console.error(lua.lua_touserdata(L, 1))
@@ -50,25 +20,32 @@ function CreateState() {
     return L
 }
 
-function Load(L, source, chunkname) {
-	if (typeof source == "string")
-		source = to_luastring(source);
-	else if (!(source instanceof Uint8Array))
-		throw new TypeError("expects an array of bytes or javascript string");
+function Load(L, Source, ChunkName) {
+    if (ChunkName) {
+        ChunkName = to_luastring(ChunkName)
+    } else {
+        ChunkName = null
+    }
 
-	chunkname = chunkname?to_luastring(chunkname):null;
-	let ok = lauxlib.luaL_loadbuffer(L, source, null, chunkname);
-	let res;
-	if (ok === lua.LUA_ERRSYNTAX) {
-		res = new SyntaxError(lua.lua_tojsstring(L, -1));
-	} else {
-		res = Interop.tojs(L, -1);
-	}
-	lua.lua_pop(L, 1);
-	if (ok !== lua.LUA_OK) {
-		throw res;
-	}
-    return res;
+    const LoadOk = lauxlib.luaL_loadbuffer(
+        L,
+        to_luastring(Source),
+        null,
+        ChunkName
+    )
+
+    var Response
+    if (LoadOk == lua.LUA_ERRSYNTAX) {
+        Response = new SyntaxError(lua.lua_tojsstring(L, -1))
+    } else {
+        Response = Interop.tojs(L, -1)
+    }
+    lua.lua_pop(L, 1)
+    if (LoadOk !== lua.LUA_OK) {
+        throw Response
+    }
+
+	return Response
 }
 
 function LoadFile(L, FilePath) {
@@ -77,11 +54,11 @@ function LoadFile(L, FilePath) {
         throw new SyntaxError(lua.lua_tojsstring(L, -1))
     }
     if (LoadOk == lua.LUA_OK) {
-        const CallOk = lua.lua_pcall(L, 0, 0, ErrorFunc)
+        const CallOk = lua.lua_pcall(L, 0, 0, 0)
         if (CallOk !== lua.LUA_OK) {
-            let e = Interop.tojs(L, -1);
+            const Error = Interop.tojs(L, -1);
             lua.lua_pop(L, 1);
-            console.error(e)
+            throw Error
         }
     }
 }
