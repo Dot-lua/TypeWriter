e20528924ab6c7fa3c0d7145c602ca1b80fefdbe
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-18 22:24:38 +0100

    Update Index.js

--- TypeWriter/Index.js
+++ TypeWriter/Index.js
@@ -2,57 +2,90 @@ const FS = require("fs-extra")
 const FSHelpers = require("./Lib/FSHelpers")
 const Path = require("path")
 
-const ArgumentData = require("./Registry/Arguments")
-const Module = require("module")
+const Logger = require("./Classes/Logger.js")
+const DepencencyManager = require("./Classes/DependencyManager/Index.js")
 
-global.TypeWriter = {}
-//Arguments
-TypeWriter.Arguments = ArgumentData.Arguments
-TypeWriter.ApplicationArguments = ArgumentData.Unknown
-//Os
-TypeWriter.OS = process.platform
-//File Paths
-TypeWriter.Folder = FSHelpers.FindUp(Path.resolve(process.argv0, "../"), "InstallationDirectory", 40) || Path.resolve(`${process.argv0}/../`)
-TypeWriter.Executable = process.execPath
-TypeWriter.ApplicationData = `${TypeWriter.Folder}/ApplicationData/`
-//Other
-TypeWriter.Logger = require("./Lib/Logger")
-TypeWriter.OriginalRequire = Module.prototype.require
-TypeWriter.PackageManagers = require("./Lib/PackageManagers/Index.js")
+class TypeWriter {
+    constructor() {
+        this.ArgumentData = require("./Registry/Arguments")
+        this.Arguments = this.ArgumentData.Arguments
+        this.ApplicationArguments = this.ArgumentData.Unknown
 
-async function Main() {
-    //Check if valid install
-    const Installer = require("./Installer/Index.js")
-    if (FS.existsSync(`${TypeWriter.Folder}/InstallationDirectory`)) {
-        TypeWriter.Logger.Debug("Valid installation found, Trying postinstall scripts")
-        await Installer.PostInstall()
-    } else {
-        TypeWriter.Logger.Information("Invalid install found")
-        await Installer.Install()
-        process.exit(0)
+        this.OS = process.platform
+        this.OriginalRequire = require("module").prototype.require
+
+        this.InstallationFolder = FSHelpers.FindUp(Path.resolve(process.argv0, "../"), "InstallationDirectory", 40) || Path.resolve(`${process.argv0}/../`)
+        this.ApplicationData = Path.normalize(`${this.InstallationFolder}/ApplicationData/`)
+        this.Executable = process.execPath
+
+        this.Logger = this.CreateLogger("TypeWriter")
+        this.DependencyManager = new DepencencyManager()
+        this.Actions = require("./Actions/List.js")
     }
 
-    //Create folders in Exe folder
-    FS.ensureDirSync(TypeWriter.ApplicationData)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Binaries/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/BuildCache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ExecuteCache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Modules/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/ModuleTars/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Unpack/`)
-    FS.ensureDirSync(`${TypeWriter.Folder}/Cache/ModuleCache/LIT/`)
+    CreateLogger(LoggerName) {
+        return new Logger(LoggerName)
+    }
+    
+    RunAction(ActionName) {
+        if (!ActionName && !this.Arguments.action) {
+            return false
+        } else {
+            ActionName = this.Arguments.action
+        }
 
-    //Run action
-    if (TypeWriter.Arguments.action) {
-        const Actions = require("./Actions/List.js")
-        const Action = Actions[TypeWriter.Arguments.action]
-        await Action.Execute()
-    } else {
-        ArgumentData.Parser.print_help()
+        const Action = this.Actions[ActionName]
+        if (!Action) {
+            this.Logger.Error(`Action '${ActionName}' not found`)
+            process.exit(1)
+        }
+
+        Action.Execute()
+        return true
+    }
+    
+    ShowHelp() {
+        this.ArgumentData.Parser.print_help()
+    }
+
+    CreateFolders() {
+        this.Folders = {
+            BinariesFolder: `${this.InstallationFolder}/Binaries/`,
+            CacheFolder: `${this.InstallationFolder}/Cache/`,
+            Cache: {
+                BuildCacheFolder: `${this.InstallationFolder}/Cache/BuildCache/`,
+                ExecuteCacheFolder: `${this.InstallationFolder}/Cache/ExecuteCache/`,
+                ModuleCacheFolder: `${this.InstallationFolder}/Cache/ModuleCache/`,
+                ModuleCache: {
+                    NPMFolder: `${this.InstallationFolder}/Cache/ModuleCache/NPM/`,
+                    NPM: {
+                        ModulesFolder: `${this.InstallationFolder}/Cache/ModuleCache/NPM/Modules/`,
+                        ModuleTarsFolder: `${this.InstallationFolder}/Cache/ModuleCache/NPM/ModuleTars/`,
+                        UnpackFolder: `${this.InstallationFolder}/Cache/ModuleCache/NPM/Unpack/`
+                    },
+                    LITFolder: `${this.InstallationFolder}/Cache/ModuleCache/LIT/`
+                }
+            }
+        }
+
+        FS.ensureDirSync(this.ApplicationData)
+        FS.ensureDirSync(this.Folders.BinariesFolder)
+        FS.ensureDirSync(this.Folders.CacheFolder)
+        FS.ensureDirSync(this.Folders.Cache.BuildCacheFolder)
+        FS.ensureDirSync(this.Folders.Cache.ExecuteCacheFolder)
+        FS.ensureDirSync(this.Folders.Cache.ModuleCacheFolder)
+        FS.ensureDirSync(this.Folders.Cache.ModuleCacheFolder)
+        FS.ensureDirSync(this.Folders.Cache.ModuleCache.NPMFolder)
+        FS.ensureDirSync(this.Folders.Cache.ModuleCache.NPM.ModulesFolder)
+        FS.ensureDirSync(this.Folders.Cache.ModuleCache.NPM.ModuleTarsFolder)
+        FS.ensureDirSync(this.Folders.Cache.ModuleCache.NPM.UnpackFolder)
+        FS.ensureDirSync(this.Folders.Cache.ModuleCache.LITFolder)
     }
 }
 
-Main()
+global.TypeWriter = new TypeWriter()
+global.TypeWriter.CreateFolders()
+
+if (!global.TypeWriter.RunAction()) {
+    global.TypeWriter.ShowHelp()
+}
