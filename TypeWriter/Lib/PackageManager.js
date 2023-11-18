d37f5557aad49004d21761a0bed600b092b6f7db
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-18 22:23:31 +0100

    rework packagemanager

--- TypeWriter/Lib/PackageManager.js
+++ TypeWriter/Lib/PackageManager.js
@@ -1,31 +1,35 @@
-const PackageManager = {}
+class PackageManager {
+    constructor() {
 
-PackageManager.GetPackageInfo = function (Id) {
-    return TypeWriter.LoadedPackages[Id].Package
-}
-
-PackageManager.GetPackageEntrypoints = function (Id) {
-    return this.GetPackageInfo(Id).Entrypoints
-}
-
-PackageManager.ListPackageIds = function () {
-    return Object.keys(TypeWriter.LoadedPackages)
-}
-
-PackageManager.IsPackageLoaded = function (Id) {
-    return this.ListPackageIds().includes(Id)
-}
-
-PackageManager.ListDependencyObjects = function(Id) {
-    const PackageData = TypeWriter.LoadedPackages[Id].Package
-    const Objects = []
-
-    for (const DependencyString of PackageData.Dependencies) {
-        const DependencyObject = TypeWriter.PackageManagers.TryParse(DependencyString)
-        Objects.push(DependencyObject)
     }
 
-    return Objects
+    GetPackageInfo(Id) {
+        return TypeWriter.LoadedPackages[Id].Package
+    }
+
+    GetPackageEntrypoints(Id) {
+        return this.GetPackageInfo(Id).Entrypoints
+    }
+
+    ListPackageIds() {
+        return Object.keys(TypeWriter.LoadedPackages)
+    }
+
+    IsPackageLoaded(Id) {
+        return this.ListPackageIds().includes(Id)
+    }
+
+    ListDependencyObjects(Id) {
+        const PackageData = TypeWriter.LoadedPackages[Id].Package
+        const Objects = []
+
+        for (const DependencyString of PackageData.Dependencies) {
+            const DependencyObject = TypeWriter.PackageManagers.TryParse(DependencyString)
+            Objects.push(DependencyObject)
+        }
+
+        return Objects
+    }
 }
 
 module.exports = PackageManager
