02bcf88115e6c8f120b3b1dfe487d79044bf9175
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-20 12:52:52 +0100

    fix fetchfile

--- TypeWriter/Lib/FetchFile.js
+++ TypeWriter/Lib/FetchFile.js
@@ -5,15 +5,14 @@ const Fetch = require("node-fetch")
 async function FetchFile(Url, File) {
     try {
         if (FS.existsSync(File)) { return }
-        const Response = await Fetch(Url)
-        const Content = await Response.text()
         FS.mkdirpSync(Path.dirname(File))
-        FS.writeFileSync(File, Content)
+        const Response = await Fetch(Url)
+        const Buffer = await Response.buffer()
+        FS.writeFileSync(File, Buffer, "utf8")
     } catch (error) {
         TypeWriter.Logger.Warning(`Failed to fetch '${Url}' (${error})`)
         return await FetchFile(Url, File)
     }
-    
 }
 
 module.exports = FetchFile
